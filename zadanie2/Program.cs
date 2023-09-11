d0c62b093285794de7ca02de8d4b2e1bb4c8c6f5
Цомкалов Игорь <igorcomkalov@gmail.com>
2023-09-11 13:16:02 +0300

    Добавьте файлы проекта.

---
+++ zadanie2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Process[] processes = Process.GetProcesses();
+        Console.WriteLine("Все процессы");
+        foreach (Process process in processes)
+        {
+            Console.WriteLine($"PID:{process.Id}, Название:{process.ProcessName}");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Первые 3 процесса");
+        for(int i=0; i < Math.Min(3,processes.Length); i++) {
+            Process process = processes[i];
+            Console.WriteLine($"PID:{process.Id}, Название:{process.ProcessName}");
+        }
+        Console.ReadKey();
+    }
+}
